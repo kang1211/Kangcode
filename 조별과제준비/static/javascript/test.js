a320d46f2546a1d2c00b2b6e98183329acfea4ca
kang1211 <zdxsca1211@naver.com>
2024-01-14 18:26:13 +0900

    hhhh

--- 조별과제준비/static/javascript/test.js
+++ 조별과제준비/static/javascript/test.js
@@ -1,27 +1,50 @@
-let slide_idx=0;
-
 $(function(){
 
+// main > main1 > 슬라이드 이동------------------------------------------------------------------------------------
+    let count=0;
+    let pgnum=1;
+    const slider = document.querySelector(".slideList");
+    var nextNum = pgnum+"/"+slider.children.length; // 페이지 번호
+    var paragraph = document.getElementById("slide_page_num") // p태그
+    paragraph.innerHTML = nextNum; // p태그에 페이지번호 넣기
 
-$(".slide_bt_left").on("click", function(){
-    if(slide_idx!=$(".slideImg").length-1){
-        slide_idx++;
-        $(".slideList").css("right",(slide_idx*1200)+"px");
-    }
-})
+    function nextSlide(){ // 페이지 다음으로 넘기기
+        count++;
+        pgnum++;
+        if(count>=slider.children.length){
+            count=0;
+            pgnum=1;
+        }
+        updateSlide();
 
-$(".slide_bt_right").on("click",function(){
-    if(slide_idx!=0){ // 첫번째 이미지일 경우 이동불가
-    slide_idx--;
-    $(".slideList").css("right",(slide_idx*1200)+"px");
+        var nextNum = pgnum+"/"+slider.children.length; // 페이지 번호
+
+        var paragraph = document.getElementById("slide_page_num") // 페이지번호 넣는 p태그
+        paragraph.innerHTML = nextNum; // 다음 페이지 번호 넣기
     }
+    
+    function prevSlide(){ // 페이지 이전으로 넘기기
+        count--;
+        pgnum--;
+        if(count<0){
+            count=slider.children.length - 1;
+            pgnum=slider.children.length;
+        }
+        updateSlide();
+
+        var nextNum = pgnum+"/"+slider.children.length; // 페이지 번호
+
+        var paragraph = document.getElementById("slide_page_num") // 페이지번호 넣는 p태그
+        paragraph.innerHTML = nextNum; // 다음 페이지 번호 넣기
+    }
+    
+    function updateSlide(){
+        slider.style.transform = `translateX(${-count * 100}%)`; // 페이지 전환 위치
+    }
+
+    // setInterval(nextSlide, 5000);
+
+    $(".slide_bt_right").on("click", nextSlide); // 페이지 오른쪽 버튼
+    $(".slide_bt_left").on("click", prevSlide); // 페이지 왼쪽 버튼
 });
 
-setInterval(
-    function(){
-        if(slide_idx!=$(".slideImg").length-1)
-        {slide_idx++;$(".slideList").css("right",(slide_idx*1200)+"px");}
-        else{slide_idx=0;$(".slideList").css("right",(slide_idx*1200)+"px");}
-            } , 5000
-            );
-})
